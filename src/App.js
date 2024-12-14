e940bff7acd660d822ca8ef8f18f2707bf500ce1
Salah Ansari <salah.env@gmail.com>
2024-12-14 13:40:38 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import LazyLoading from './components/LazyLoading';
-import { Store } from './Store';
-import StoreDemo from './components/StoreDemo';
-
-
-function StoreDemoWrapper() {
-  return (
-    <Store>
-      <StoreDemo/>
-    </Store>
-  )
-}
-
-
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lazy-loading" element={<LazyLoading />} />
-        <Route path="/store" element={<StoreDemoWrapper />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
