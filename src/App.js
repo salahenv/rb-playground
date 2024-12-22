5686c543287299d0d9bca073bb8a640f9674fd44
Salah Ansari <salah.env@gmail.com>
2024-12-22 15:29:13 +0530

    added: n*n tic toe

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import CaraouselPage from './Carausel';
+import Game from './components/tic-toe';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/carousel" element={<CaraouselPage />} />
-
+      <Route path="/game" element={<Game n={3} />} />
     </Routes>
   );
 }
